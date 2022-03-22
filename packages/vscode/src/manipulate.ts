9dc99a392e6c2c2b9c9ba3aae8b17db86287bddb
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-22 23:25:37 +0800

    feat(vscode): improve moving

--- packages/vscode/src/manipulate.ts
+++ packages/vscode/src/manipulate.ts
@@ -1,6 +1,6 @@
-import { Snapshots } from 'retypewriter'
-import type { TextDocument } from 'vscode'
-import { Range, window } from 'vscode'
+import { Snapshots, parseSnapshots } from 'retypewriter'
+import type { TextDocument, TextEditor } from 'vscode'
+import { Range, Selection, window } from 'vscode'
 import { updateAnnotation } from './decoration'
 import { manager } from './manager'
 
@@ -15,20 +15,39 @@ export async function manipulateSnapshotsInDocument(
 
   manager.set(doc.uri.fsPath, snaps)
   const result = snaps.toString()
+  let editor: TextEditor | undefined
   if (result !== raw) {
-    const editor = await window.showTextDocument(doc, { preview: false, preserveFocus: false })
+    editor = await window.showTextDocument(doc, { preview: false, preserveFocus: false })
     await editor.edit((e) => {
       e.replace(new Range(0, 0, Infinity, Infinity), snaps.toString())
     })
     await updateAnnotation(editor)
   }
+
+  return {
+    snaps,
+    doc,
+    editor,
+  }
 }
 
 export async function move(doc: TextDocument, index: number, delta = 1) {
-  return manipulateSnapshotsInDocument(
+  const to = index + delta
+  const { editor } = await manipulateSnapshotsInDocument(
     doc,
-    snaps => snaps.move(index, index + delta),
+    snaps => snaps.move(index, to),
   )
+
+  if (editor) {
+    // select to highlight the moved block
+    const target = parseSnapshots(doc.getText()).snapshots[to]
+    if (target) {
+      editor.selection = new Selection(
+        doc.positionAt(target.start + 1),
+        doc.positionAt(target.end),
+      )
+    }
+  }
 }
 
 export const moveUp = (doc: TextDocument, index: number) => move(doc, index, -1)
