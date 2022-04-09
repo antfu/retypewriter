714b3598abb46a73c743a19121f3693e3c076907
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-09 13:52:34 +0800

    chore: ci

--- packages/core/test/parse.test.ts
+++ packages/core/test/parse.test.ts
@@ -1,6 +1,6 @@
 import { expect, it } from 'vitest'
 import { parseSnapshots } from '../src/state/parse'
-import fixture from '../../../examples/main.ts.retypewriter?raw'
+import fixture from '../../../examples/main.js.retypewriter?raw'
 
 it('works', () => {
   expect(parseSnapshots(fixture)).toMatchSnapshot()
